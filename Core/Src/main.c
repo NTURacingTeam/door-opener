d504a09a41e731723f234f91e1adf6c2f4078f32
Tsou <astatine1184@gmail.com>
2024-05-06 18:20:15 +0800

    add temp error handler

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -302,6 +302,7 @@ void Error_Handler(void)
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
+  HAL_GPIO_WritePin(RED_GPIO_Port, RED_Pin, GPIO_PIN_SET);
   while (1)
   {
   }
